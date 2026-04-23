703c0ffe4a05cc882318390de71c33e03134357c
Fabiana Bonfim <fabianabonfimdasilva29@gmail.com>
2026-04-22 22:23:20 -0300

    atualiza final2

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Olá mundo da Fabi, bom dia! Tudo bem?";
+echo "Olá mundo da Fabi, bom dia! Tudo bem?Espero que sim"
 $a = readline ("digite um valor que voce acha que vale:");
     echo "o valor digitado foi: $a";   
     
