298dc1e6c96095116ba7b3146c27851dab193780
unknown <fabianabonfimdasilva29@gmail.com>
2026-05-06 21:38:18 -0300

    commit testel

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Olá mundo da Fabi, boa noite! Tudo bem? Espero que sim";
+echo "Olá mundo da Fabi, boa noite! Tudo bem? Bora pra mais um dia";
 $a = readline ("digite um valor que voce acha que vale:");
     echo "o valor digitado foi: $a";   
     
