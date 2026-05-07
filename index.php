7bfff56636c1c0ec91c3ecb66862505ac25f3e8c
unknown <fabianabonfimdasilva29@gmail.com>
2026-05-06 21:51:28 -0300

    testando

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-echo "Olá mundo da Fabi, boa noite! Tudo bem? Bora pra mais um dia";
+echo "Olá mundo da Fabi, boa noite! Tudo bem? Bora pra mais um dia! Aula GitHub pela segunda vez";
 $a = readline ("digite um valor que voce acha que vale:");
     echo "o valor digitado foi: $a";   
     
